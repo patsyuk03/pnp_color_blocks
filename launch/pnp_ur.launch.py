5f0fbda8113bcd15d9616534d565dd1e82e612f5
patsyuk03 <iryna.gurova@gmail.com>
2023-10-16 16:41:37 +0300

    tested castom moveit config

--- launch/pnp_ur.launch.py
+++ launch/pnp_ur.launch.py
@@ -51,20 +51,20 @@ def generate_launch_description():
     ld.add_action(TimerAction(period=3.0,
         actions=[IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                os.path.join(get_package_share_directory('ur_gripper_moveit_config'), 'launch/demo.launch.py')
+                os.path.join(get_package_share_directory('ur_robotiq_moveit_config'), 'launch/demo.launch.py')
             )
         )]
     ))
 
-    # ld.add_action(IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(get_package_share_directory('robotiq_hande_ros2_driver'), 'launch/gripper_bringup.launch.py')
-    #     ),
-    #     launch_arguments={
-    #         'robot_ip': robot_ip,
-    #     }.items(),
-    #     condition=IfCondition(add_gripper)
-    # ))
+    ld.add_action(IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('robotiq_hande_ros2_driver'), 'launch/gripper_bringup.launch.py')
+        ),
+        launch_arguments={
+            'robot_ip': robot_ip,
+        }.items(),
+        condition=IfCondition(add_gripper)
+    ))
 
     # ld.add_action(Node(
     #     package="pnp_color_blocks",
